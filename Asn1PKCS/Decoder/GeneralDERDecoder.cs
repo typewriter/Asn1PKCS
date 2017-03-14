bc5b24032c7fa00e5ec527542af71aae43ee432b
typewriter <takuyaxp@gmail.com>
2017-03-14 21:40:52 +0900

    Support: PKCS #1 RSA key decode (include test)

---
+++ Asn1PKCS/Decoder/GeneralDERDecoder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asn1PKCS.Decoder
+{
+    /// <summary>
+    /// General DER Decoder
+    /// </summary>
+    internal class GeneralDERDecoder
+    {
+        /// <summary>
+        /// Extract "INTEGER" values recursively.
+        /// "INTEGER"値を抽出します。SEQUENCEなどの内側も再帰的に抽出します。
+        /// </summary>
+        /// <param name="derData"></param>
+        /// <returns></returns>
+        internal static List<byte[]> ExtractIntegerDatas(byte[] derData)
+        {
+            List<byte[]> intDataList = new List<byte[]>();
+
+            int index = 0;
+            while (index < derData.Length)
+            {
+                byte tagType = derData[index];
+                index++;
+
+                int length = derData[index];
+                index++;
+
+                // length >= 128
+                if ((length & 0x80) != 0)
+                {
+                    int lengthLength = length & 0x7F;
+                    byte[] lengthBytes = 
+                        new byte[4 - lengthLength] // padding (int32)
+                        .Concat(derData.Skip(index).Take(lengthLength)).ToArray();
+
+                    if (BitConverter.IsLittleEndian)
+                    {
+                        Array.Reverse(lengthBytes);
+                    }
+                    length = BitConverter.ToInt32(lengthBytes, 0);
+                    index += lengthLength;
+                }
+
+                // HACK: DERUtilsを使うべきで、DERUtilsは名前空間を移動したほうがよい
+                if (tagType == 0x02)
+                {
+                    // INTEGER values.
+                    byte[] value = derData.Skip(index).Take(length).ToArray();
+                    intDataList.Add(value);
+                    index += length;
+                }
+                else if (tagType == 0x03 || tagType == 0x04 || tagType == 0x30)
+                {
+                    // BITSTRING, OCTETSTRING, SEQUENCE values.
+
+                }
+                else
+                {
+                    // ELSE
+                    index += length;
+                }
+            }
+
+            return intDataList;
+        }
+    }
+}
---
+++ Asn1PKCS/Decoder/PKCS1DERDecoder.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asn1PKCS.Decoder
+{
+    /// <summary>
+    /// PKCS #1 RSA key ASN.1 Decoder
+    /// </summary>
+    public class PKCS1DERDecoder
+    { 
+        /// <summary>
+        /// Decode RSAPublicKey in PKCS #1(DER) format.
+        /// PKCS #1(DER)形式のRSA公開鍵をデコードします。
+        /// </summary>
+        /// <param name="derEncodedBytes"></param>
+        /// <returns></returns>
+        public static RSAParameters DecodePublicKey(byte[] derEncodedBytes)
+        {
+            List<byte[]> intItems = GeneralDERDecoder.ExtractIntegerDatas(derEncodedBytes);
+
+            RSAParameters rsaParams = new RSAParameters();
+            rsaParams.Modulus = intItems[0];
+            rsaParams.Exponent = intItems[1];
+
+            return rsaParams;
+        }
+
+        /// <summary>
+        /// Decode RSAPublicKey in PKCS #1(DER) format.
+        /// PKCS #1(DER)形式のRSA公開鍵をデコードします。
+        /// </summary>
+        /// <param name="derBase64EncodedString"></param>
+        /// <returns></returns>
+        public static RSAParameters DecodePublicKey(string derBase64EncodedString)
+        {
+            return DecodePublicKey(Convert.FromBase64String(derBase64EncodedString));
+        }
+
+        /// <summary>
+        /// Decode RSAPrivateKey in PKCS #1(DER) format.
+        /// PKCS #1(DER)形式のRSA秘密鍵をデコードします。
+        /// </summary>
+        /// <param name="derEncodedBytes"></param>
+        /// <returns></returns>
+        public static RSAParameters DecodePrivateKey(byte[] derEncodedBytes)
+        {
+            List<byte[]> intItems = GeneralDERDecoder.ExtractIntegerDatas(derEncodedBytes);
+
+            RSAParameters rsaParams = new RSAParameters();
+            
+            rsaParams.Modulus = intItems[1];
+            rsaParams.Exponent = intItems[2];
+            rsaParams.D = intItems[3];
+            rsaParams.P = intItems[4];
+            rsaParams.Q = intItems[5];
+            rsaParams.DP = intItems[6];
+            rsaParams.DQ = intItems[7];
+            rsaParams.InverseQ = intItems[8];
+
+            return rsaParams;
+        }
+
+        /// <summary>
+        /// Decode RSAPrivateKey in PKCS #1(DER) format.
+        /// PKCS #1(DER)形式のRSA秘密鍵をデコードします。
+        /// </summary>
+        /// <param name="derBase64EncodedString"></param>
+        /// <returns></returns>
+        public static RSAParameters DecodePrivateKey(string derBase64EncodedString)
+        {
+            return DecodePrivateKey(Convert.FromBase64String(derBase64EncodedString));
+        }
+    }
+}
